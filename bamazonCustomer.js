ed2824a2fdda1fba279122ae055faef25fa57c47
Christopher Gottshalk Jr <christophergottshalkjr@gmail.com>
2019-11-14 23:17:35 -0500

    added prompt for user to choose what they would like to purchase and how much

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 require("dotenv").config();
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -26,13 +27,41 @@ function showItems() {
         if (err) throw err;
         console.log("----------------------------------");
         for (var i = 0; i < results.length; i++) {
-            
-            console.log("Item #: " + results[i].item_id + " - " + results[i].product_name + " - " + results[i].department_name + " - " + "$" + results[i].price + " - " +  "qty: " + results[i].stock_quantity);
-            
+
+            console.log("Item #: " + results[i].item_id + " - " + results[i].product_name + " - " + results[i].department_name + " - " + "$" + results[i].price + " - " + "qty: " + results[i].stock_quantity);
+
         }
         console.log("----------------------------------");
     });
-    
+    userPrompt();
+}
+
+function userPrompt() {
+
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+
+        inquirer
+            .prompt([
+                {
+                    name: "choice",
+                    type: "rawlist",
+                    message: "What is the ID# of the product you would like to purchace?",
+                    choices: function () {
+                        var productArr = [];
+                        for (var i = 0; i < res.length; i++) {
+                            productArr.push(res[i].item_id)
+                        }
+                        return productArr;
+                    }
+                }, {
+                    name: "qtySelection",
+                    message: "How many units of would you like?",
+                    type: "input"
+                },
+            ]);
+    });
+    connection.end();
 }
 
 
@@ -41,4 +70,6 @@ function showItems() {
 
 
 
-// connection.end();
+
+
+
